483bc1ac1885ca0412e57de66c2af978f2c527b3
xbl <x_bl@sina.com>
2022-11-01 13:01:21 +0800

    feat: 添加发布功能

---
+++ forge.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  packagerConfig: {
+    icon: 'icons/logo.ico',
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'pdf_watermark',
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: [
+        'darwin',
+      ],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'xbl',
+          name: 'PW',
+        },
+        prerelease: true,
+      },
+    },
+  ],
+};
