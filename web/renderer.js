0dd8d664738b6dcb68446862b8f848a36d1367aa
xbl <x_bl@sina.com>
2023-01-31 12:21:06 +0800

    refactor: 抽出默认变量和函数
    抽出updateTextOptions 函数，以便于后面做测试

--- web/renderer.js
+++ web/renderer.js
@@ -1,36 +1,28 @@
 /* eslint-disable-next-line no-undef */
 const { createApp } = Vue;
 
+const defaultTextOptions = {
+  inputFilePath: '',
+  text: '',
+  textSize: 30,
+  opacity: 0.4,
+  rotate: -45,
+  marginRight: 150,
+  marginBottom: 150,
+  style: 'tiled',
+};
+
 const vueApp = createApp({
   data() {
     return {
       loading: false,
       showTip: false,
-      textOptions: {
-        inputFilePath: '',
-        text: '',
-        textSize: 30,
-        opacity: 0.4,
-        rotate: -45,
-        marginRight: 150,
-        marginBottom: 150,
-        style: 'tiled',
-      },
+      textOptions: defaultTextOptions,
     };
   },
   watch: {
     'textOptions.style': function (newVal) {
-      if (newVal === 'centered') {
-        this.textOptions.rotate = 0;
-        this.textOptions.textSize = 150;
-        this.textOptions.marginRight = 50;
-        this.textOptions.marginBottom = 50;
-        return;
-      }
-      this.textOptions.rotate = -45;
-      this.textOptions.textSize = 30;
-      this.textOptions.marginRight = 150;
-      this.textOptions.marginBottom = 150;
+      this.updateTextOptions(newVal);
     },
   },
   methods: {
@@ -50,6 +42,19 @@ const vueApp = createApp({
       setTimeout(() => {
         this.showTip = false;
       }, 3000);
+    },
+    updateTextOptions(newVal) {
+      if (newVal === 'centered') {
+        this.textOptions.rotate = 0;
+        this.textOptions.textSize = 150;
+        this.textOptions.marginRight = 50;
+        this.textOptions.marginBottom = 50;
+        return;
+      }
+      this.textOptions.rotate = -45;
+      this.textOptions.textSize = 30;
+      this.textOptions.marginRight = 150;
+      this.textOptions.marginBottom = 150;
     },
   },
 });
